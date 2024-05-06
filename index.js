32bfc63ef0584fd51485fc71afb3c5f653d426ee
gyeuijrge <dsjcerekcrowe21@hotmail.com>
2024-05-07 02:44:15 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
+function solveSudoku(board) {
+  solve(board);
+  function solve(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const char = num.toString();
+            if (isValid(board, i, j, char)) {
+              board[i][j] = char;
+              if (solve(board)) return true;
+              board[i][j] = ".";
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
   }
-  return currMax;
+  function isValid(board, row, col, char) {
+    for (let i = 0; i < 9; i++) {
+      if (
+        board[row][i] === char ||
+        board[i][col] === char ||
+        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][
+          3 * Math.floor(col / 3) + (i % 3)
+        ] === char
+      )
+        return false;
+    }
+    return true;
+  }
 }
