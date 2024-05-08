6231321669b0e2773b4b0e7c62540912d5da52ee
gyeuijrge <dsjcerekcrowe21@hotmail.com>
2024-05-08 12:17:37 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function allPathsSourceTarget(graph) {
-  const result = [];
-  const target = graph.length - 1;
-  dfs(graph, 0, [0]);
-  function dfs(graph, node, path) {
-    if (node === target) {
-      result.push([...path]);
-      return;
-    }
-    for (const neighbor of graph[node]) {
-      path.push(neighbor);
-      dfs(graph, neighbor, path);
-      path.pop();
-    }
-  }
-  return result;
+function getMax(arr) {
+  return Math.max(...arr);
 }
