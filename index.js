453736f3512d58f7f893a71f11c7859420fe1517
gyeuijrge <dsjcerekcrowe21@hotmail.com>
2024-05-09 08:49:14 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function getMax(arr) {
-  return Math.max(...arr);
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
 }
